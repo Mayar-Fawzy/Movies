914bf9fe313d9ac9d1e03a0d638a518609cb851d
Mayar-Fawzy <momayar920@gmail.com>
2024-09-26 22:51:54 +0200

    validation Register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { MoviesComponent } from './movies/movies.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { TvComponent } from './tv/tv.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'movies',component:MoviesComponent},
+  {path:'tv',component:TvComponent},
+  {path:'contacts',component:ContactsComponent},
+  
+  {path:'login',component:LoginComponent},
+  
+ 
+  {path:'register',component:RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
