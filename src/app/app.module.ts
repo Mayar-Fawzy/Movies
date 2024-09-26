540c216c0d56a38db45461041eda78e5736749e8
Mayar-Fawzy <momayar920@gmail.com>
2024-09-26 17:45:38 +0200

    generate commponent and install bootstrap and fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsComponent,
+    GalleryComponent,
+    AboutComponent,
+    LoginComponent,
+    MoviesComponent,
+    MoviesDetailsComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
