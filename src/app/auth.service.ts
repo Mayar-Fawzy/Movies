89623790a5cd7d9494f8d23a53c7dd166b607bb2
Mayar-Fawzy <momayar920@gmail.com>
2024-09-29 21:00:03 +0200

    Grud Router

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Users } from './users';
 import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
    
   userData=new BehaviorSubject(null);
 
-  constructor(private _HttpClient:HttpClient) { }
+  constructor(private _HttpClient:HttpClient,private _Router:Router) { }
     serUserData():void{
       //فكيت تشفير ال token
       let encodedToken=JSON.stringify(localStorage.getItem('userToken'));
@@ -26,5 +27,10 @@ export class AuthService {
   Login(userdata:object):Observable<any>
   {
     return  this._HttpClient.post('https://ecommerce.routemisr.com/api/v1/auth/signin',userdata)
+  }
+  logOut():void{
+    localStorage.removeItem('userToken');
+    this.userData.next(null);
+    this._Router.navigate(['login'])
   }
 }
--- src/app/log-out/log-out.component.ts
+++ src/app/log-out/log-out.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./log-out.component.scss']
 })
 export class LogOutComponent {
-
+   
 }
